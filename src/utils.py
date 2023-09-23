28070fb30e48aec84891cf653d4bd4f3a90c150d
Devansh Tripathi <72065825+Adroit10@users.noreply.github.com>
2023-09-23 15:09:01 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import pandas as pd
 import numpy as np
 import pickle
+from sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
 from src.logger import logging
 from src.exception import CustomException
 
@@ -14,4 +15,34 @@ def save_object(file_path,obj):
             pickle.dump(obj,file_obj)
             
     except Exception as e:
+        raise CustomException(e,sys)
+
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+        for i in range(len(models)):
+            model=list(models.values())[i]
+            # train the model
+            model.fit(X_train,y_train)
+    
+            #predict the test data
+            y_pred=model.predict(X_test)
+            ## get the score for the model
+    
+            test_model_score=r2_score(y_test,y_pred)
+            report[list(models.keys())[i]]=test_model_score
+    
+        return report
+    
+    
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+        
+    except Exception as e:
+        logging.info('Exception occured in load_object() in utils')
         raise CustomException(e,sys)
